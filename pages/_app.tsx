d4677fb0c8ad4f371dc091bf8f642870aba666ec
Robert Guss <robertguss@pm.me>
2021-11-26 09:31:49 -0500

    added custom styles and tailwind config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
-import "../styles/global.css"
+import "../styles/main.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
