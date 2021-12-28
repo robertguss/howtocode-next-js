2a01d49b8f8719320b6378b64905f0e4f8fbf6f5
Robert Guss <robertguss@pm.me>
2021-12-28 12:51:43 -0500

    added cypress tests from astro site

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,8 +4,4 @@ Cypress.Commands.add("getBySel", (selector, ...args) => {
   return cy.get(`[data-test=${selector}]`, ...args)
 })
 
-Cypress.Commands.add("getBySelLike", (selector, ...args) => {
-  return cy.get(`[data-test*=${selector}]`, ...args)
-})
-
 export {}
