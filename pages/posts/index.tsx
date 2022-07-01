a3f3dcee97a16174a3d9ab3d6afae47d01e2ad6c
Robert Guss <robertguss@pm.me>
2022-07-01 11:06:02 -0400

    removed search spec file

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -39,6 +39,7 @@ export default function Articles({ allPosts }) {
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
               className="block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              data-test="article-search-input"
             />
             <svg
               className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
